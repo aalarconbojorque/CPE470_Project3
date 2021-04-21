a4487d957a457f4bdc7044dd4c40cc91f59f3542
Andy <aa.alarcon10@gmail.com>
2021-04-21 12:17:06 -0700

    Init

---
+++ proj3.py
@@ -0,0 +1,36 @@
+# -----------------------------------------------------------------------------
+# FILE NAME:         proj3.py
+# USAGE:             python3 proj3.py
+# NOTES:             Requires NumPy installation
+#                    Requires Python3
+#
+# MODIFICATION HISTORY:
+# Author             Date           Modification(s)
+# ----------------   -----------    ---------------
+# Andy Alarcon       04-01-2021     1.0 ... Setup dev environment, imported NumPy
+# -----------------------------------------------------------------------------
+
+import numpy.matlib as m
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+import random
+
+
+def main():
+    plt.plot([1, 2, 3, 4])
+    plt.ylabel('some numbers')
+    plt.show()
+    print("Data for target, robot and error written")
+    
+
+    
+
+# ----------------------------------------------------------------------------
+# FUNCTION NAME:     CompMagSqr()
+# PURPOSE:           Computes the magnitude squared
+# -----------------------------------------------------------------------------
+
+    
+if __name__ == "__main__":
+    main()
