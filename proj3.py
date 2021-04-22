4cf4753fd899ba837c05e1c3af9b6c8a29560fa2
Andy <aa.alarcon10@gmail.com>
2021-04-22 12:17:09 -0700

    Mid

--- proj3.py
+++ proj3.py
@@ -11,6 +11,7 @@
 # -----------------------------------------------------------------------------
 
 import matplotlib.pyplot as plt
+import networkx as nx
 import numpy as np
 import math
 import random
@@ -30,12 +31,53 @@ def main():
     #Find the neighbors of the nodes
     Neigbors = FindNeighbors(nodes, r , n, delta_t_update)
 
-    plt.plot([1, 2, 3, 4])
-    plt.ylabel('some numbers')
+
+    G = nx.Graph()
+
+    #Add edges
+    for i, item in enumerate(Neigbors, start=0):
+        G.addEdge(Neigbors[i])
+
+    for i, item in enumerate(nodes, start=0):
+        print("Node ", i , " X :" ,nodes[i][0], " Y :", nodes[i][1])
+
+    nod = {node: node for node in nodes}
+
+    fig, ax = plt.subplots()
+    nx.draw(G, nod=nod, node_color='k', ax=ax)
+    nx.draw(G, nod=nod, node_size=1500, ax=ax)
+    nx.draw_networkx_labels(G, nod=nod) 
+    plt.axis("on")
+    ax.set_xlim(0, 11)
+    ax.set_ylim(0,11)
+    ax.tick_params(left=True, bottom=True, labelleft=True, labelbottom=True)
     plt.show()
+
+   # G.visualize()
+
     print("Data for target, robot and errhhor written")
     
-
+# class GraphVisualization:
+   
+#     def __init__(self):        
+#         self.points = []
+#         self.edges = []
+          
+#     # addEdge function inputs the vertices of an
+#     # edge and appends it to the visual list
+#     def addEdge(self, pair):
+#         G.add_edge(pair)
+          
+#     # In visualize function G is an object of
+#     # class Graph given by networkx G.add_edges_from(visual)
+#     # creates a graph with a given list
+#     # nx.draw_networkx(G) - plots the graph
+#     # plt.show() - displays the graph
+#     def visualize(self):
+#         G = nx.Graph()
+#         G.add_edges_from(self.visual)
+#         nx.draw_networkx(G)
+#         plt.show()
     
 
 # ----------------------------------------------------------------------------
@@ -56,8 +98,8 @@ def FindNeighbors(nodes, r, n, delta_t):
             #If within range they are neighbors
             if(neighborDistance <= r and neighborDistance != 0):
                 Neigbor_array.append(np.array([i,j]))
-                print("Node " , i , "->" , j)
-                print("Distance :" , np.sqrt(np.square(nodes[j][0] - nodes[i][0]) + np.square(nodes[j][1] - nodes[i][1])))
+                #print("Node " , i , "->" , j)
+                #print("Distance :" , np.sqrt(np.square(nodes[j][0] - nodes[i][0]) + np.square(nodes[j][1] - nodes[i][1])))
 
     return Neigbor_array
 
