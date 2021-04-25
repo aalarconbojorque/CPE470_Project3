acb28c2c11727e85782de574530490f5446a5301
Andy <aa.alarcon10@gmail.com>
2021-04-25 12:23:29 -0700

    Made a graph object class

--- proj3.py
+++ proj3.py
@@ -1,13 +1,15 @@
 # -----------------------------------------------------------------------------
 # FILE NAME:         proj3.py
 # USAGE:             python3 proj3.py
-# NOTES:             Requires NumPy installation
+# NOTES:             Requires matplotlib and numpy installation
+#                    Requires networkx installation
 #                    Requires Python3
 #
 # MODIFICATION HISTORY:
 # Author             Date           Modification(s)
 # ----------------   -----------    ---------------
-# Andy Alarcon       04-01-2021     1.0 ... Setup dev environment, imported NumPy
+# Andy Alarcon       04-23-2021     1.0 ... Setup dev environment, imported NumPy
+# Andy Alarcon       04-25-2021     1.1 ... imported matpotlib and networkx, created graph display
 # -----------------------------------------------------------------------------
 
 import matplotlib.pyplot as plt
@@ -16,6 +18,23 @@ import numpy as np
 import math
 import random
 
+class GraphNode:
+    def __init__(self, index, position, neighbors):
+        self.index = index
+        self.position = position
+        self.neighbors = neighbors
+
+    def FindyourNeighbors(self, nodes, r):
+      for j, item in enumerate(nodes, start=0):
+            # Calculate distance between self and j
+            neighborDistance = np.sqrt(np.square(nodes[j][0] - self.position[0]) + np.square(nodes[j][1] - self.position[1]))
+            # If within range they are neighbors
+            if(neighborDistance <= r and neighborDistance != 0):
+                self.neighbors.append(j)
+               # print("Node ", self.index, "->", j)
+                
+
+
 
 def main():
     r = 1  # Set Communication Range
@@ -23,11 +42,18 @@ def main():
     delta_t_update = 0.008
     n = 2  # Number of dimensions aaaddddddd dsssdd ffff ffff
     nodes = np.random.randn(num_nodes, n)
+    nodesObjects = []
 
     # Add measurment for each node yi = theta_t + v_i
     nodes_va = (50 * np.ones((num_nodes, 1))) + \
         (1 * np.random.randn(num_nodes, 1))
     nodes_va0 = nodes_va  # Save inital measurments
+
+    for i, item in enumerate(nodes, start=0):
+        nodesObjects.append(GraphNode(i, np.array([nodes[i][0], nodes[i][1]]), []))
+        nodesObjects[i].FindyourNeighbors(nodes, r)
+
+    
 
     # Find the neighbors of the nodes
     Neigbors = FindNeighbors(nodes, r, n, delta_t_update)
@@ -80,6 +106,7 @@ def DisplayGraph(nodes, Neigbors):
 # -----------------------------------------------------------------------------
 def FindNeighbors(nodes, r, n, delta_t):
 
+    #Tempt array for neigbors
     Neigbor_array = []
 
     # Loop through each node and compare it to all other nodes
