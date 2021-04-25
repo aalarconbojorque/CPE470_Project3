b4ff4fc0605224766a2e9b56b620b092e5073af6
Andy <aa.alarcon10@gmail.com>
2021-04-25 09:42:22 -0700

    graphing complete

--- proj3.py
+++ proj3.py
@@ -31,58 +31,42 @@ def main():
     #Find the neighbors of the nodes
     Neigbors = FindNeighbors(nodes, r , n, delta_t_update)
 
-
+    #Create grpah object
     G = nx.Graph()
 
-    #Add edges
-    for i, item in enumerate(Neigbors, start=0):
-        G.addEdge(Neigbors[i])
+    # #Display x and y coor
+    # for i, item in enumerate(nodes, start=0):
+    #     print("Node ", i , " X :" ,nodes[i][0], " Y :", nodes[i][1])
 
+    #Add nodes to the graph
     for i, item in enumerate(nodes, start=0):
-        print("Node ", i , " X :" ,nodes[i][0], " Y :", nodes[i][1])
+        G.add_node(str(i), pos=(nodes[i][0], nodes[i][1]))
 
-    nod = {node: node for node in nodes}
-
+    #Add edges to the graph
+    for i, item in enumerate(Neigbors, start=0):
+        G.add_edge(str(Neigbors[i][0]), str(Neigbors[i][1]))
+  
+    #Draw graph object
+    nx.draw(G, nx.get_node_attributes(G, 'pos'), with_labels=True, node_size=250)  
+    
+    #Add x and y axis ticks and labels
     fig, ax = plt.subplots()
-    nx.draw(G, nod=nod, node_color='k', ax=ax)
-    nx.draw(G, nod=nod, node_size=1500, ax=ax)
-    nx.draw_networkx_labels(G, nod=nod) 
-    plt.axis("on")
-    ax.set_xlim(0, 11)
-    ax.set_ylim(0,11)
-    ax.tick_params(left=True, bottom=True, labelleft=True, labelbottom=True)
-    plt.show()
+    limits=plt.axis('on')
+    ax.tick_params(left=True, bottom=True, labelleft=True, labelbottom=True) 
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.title("Graph")
 
-   # G.visualize()
+    #Display graph
+    plt.show()
+    G.visualize()
 
     print("Data for target, robot and errhhor written")
     
-# class GraphVisualization:
-   
-#     def __init__(self):        
-#         self.points = []
-#         self.edges = []
-          
-#     # addEdge function inputs the vertices of an
-#     # edge and appends it to the visual list
-#     def addEdge(self, pair):
-#         G.add_edge(pair)
-          
-#     # In visualize function G is an object of
-#     # class Graph given by networkx G.add_edges_from(visual)
-#     # creates a graph with a given list
-#     # nx.draw_networkx(G) - plots the graph
-#     # plt.show() - displays the graph
-#     def visualize(self):
-#         G = nx.Graph()
-#         G.add_edges_from(self.visual)
-#         nx.draw_networkx(G)
-#         plt.show()
-    
 
 # ----------------------------------------------------------------------------
-# FUNCTION NAME:     CompMagSqr()
-# PURPOSE:           Computes the magnitude squared
+# FUNCTION NAME:     FindNeighbors()
+# PURPOSE:           Computes the neighbors of an array of nodes
 # -----------------------------------------------------------------------------
 def FindNeighbors(nodes, r, n, delta_t):
 
@@ -90,7 +74,28 @@ def FindNeighbors(nodes, r, n, delta_t):
 
     #Loop through each node and compare it to all other nodes
     for i, item in enumerate(nodes, start=0): 
-        print("Looking at point " , i)
+        for j, item in enumerate(nodes, start=0):
+
+            #Calculate distance between node i and j
+            neighborDistance = np.sqrt(np.square(nodes[j][0] - nodes[i][0]) + np.square(nodes[j][1] - nodes[i][1]))
+            #If within range they are neighbors
+            if(neighborDistance <= r and neighborDistance != 0):
+                Neigbor_array.append(np.array([i,j]))
+                #print("Node " , i , "->" , j)
+                #print("Distance :" , np.sqrt(np.square(nodes[j][0] - nodes[i][0]) + np.square(nodes[j][1] - nodes[i][1])))
+
+    return Neigbor_array
+    
+# ----------------------------------------------------------------------------
+# FUNCTION NAME:     FindNeighbors()
+# PURPOSE:           Computes the neighbors of an array of nodes
+# -----------------------------------------------------------------------------
+def FindNeighbors(nodes, r, n, delta_t):
+
+    Neigbor_array = []
+
+    #Loop through each node and compare it to all other nodes
+    for i, item in enumerate(nodes, start=0): 
         for j, item in enumerate(nodes, start=0):
 
             #Calculate distance between node i and j
